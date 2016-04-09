855726871c2b96b7cd3239afe5be38deef702f1b
Thomas Matthias <t.matthias29@gmail.com>
2016-04-08 17:24:14 -0700

    Programmatic Sizing

--- iOS-Soundscape/ViewController.swift
+++ iOS-Soundscape/ViewController.swift
@@ -7,6 +7,11 @@ class ViewController: UIViewController {
     
     // Init Multi Playback OpenAL Object
     var player = oalPlayback_MultiTest();
+    let listener_image = UIImage(named: "Headphone.png") as UIImage?
+    let upper_left_image = UIImage(named: "rain_thunder.png") as UIImage?
+    let lower_right_image = UIImage(named: "jungle_birds.png") as UIImage?
+    // let view_width = UIScreen.mainScreen().bounds.size.width
+    // let view_height = UIScreen.mainScreen().bounds.size.height
     
     override func loadView() {
         
@@ -15,27 +20,47 @@ class ViewController: UIViewController {
         
         // Create Gestures
         let panner = UIPanGestureRecognizer(target: self, action: "handlePan:")
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: "onCustomTap:")
+        let tapper = UITapGestureRecognizer(target: self, action: "onCustomTap:")
         
         // Add Icons
-        let jungle_birds_view = UIImageView(image: UIImage(named: "jungle_birds.png"))
-        jungle_birds_view.frame = CGRect(x: 300, y:600, width: 50, height: 50)
-        view.addSubview(jungle_birds_view)
+        let upper_left_source_icon = UIImageView(image: upper_left_image)
+        upper_left_source_icon.frame = CGRect(x: 0, y:0, width: 50, height: 50)
+        upper_left_source_icon.center = CGPointMake(
+            UIScreen.mainScreen().bounds.size.width / 4,
+            UIScreen.mainScreen().bounds.size.height / 4
+        )
+        upper_left_source_icon.addGestureRecognizer(tapper)
+        view.addSubview(upper_left_source_icon)
+        player.sourcePos = upper_left_source_icon.center
         
-        let rain_thunder_view = UIImageView(image: UIImage(named: "rain_thunder.png"))
-        rain_thunder_view.frame = CGRect(x: 25, y: 25, width: 50, height: 50)
-        view.addSubview(rain_thunder_view)
+        let lower_right_source_icon = UIImageView(image: lower_right_image)
+        lower_right_source_icon.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        lower_right_source_icon.center = CGPointMake(
+            3*(UIScreen.mainScreen().bounds.size.width / 4),
+            3*(UIScreen.mainScreen().bounds.size.height / 4)
+        )
+        lower_right_source_icon.addGestureRecognizer(tapper)
+        view.addSubview(lower_right_source_icon)
+        player.sourcePos2 = lower_right_source_icon.center
+        
+        // Listener Button
+        let listener_button = UIButton(type: UIButtonType.Custom) as UIButton
+        listener_button.frame = CGRect(x:0,y:0, width: 50, height: 50)
+        listener_button.center = CGPointMake(UIScreen.mainScreen().bounds.size.width / 2, UIScreen.mainScreen().bounds.size.height/2)
+        listener_button.setImage(listener_image, forState: .Normal)
+        listener_button.addGestureRecognizer(panner)
+        view.addSubview(listener_button)
+        player.listenerPos = listener_button.center
         
         // Add Moveable Icon For Listener Attachment
-        let imgView = UIImageView(image: UIImage(named: "Headphone.png"))
-        imgView.frame = CGRect(x: 150, y: 300, width: 50, height: 50)
-        imgView.backgroundColor = UIColor.redColor()
-        imgView.userInteractionEnabled = true
-        imgView.addGestureRecognizer(panner)
-        imgView.addGestureRecognizer(tapGestureRecognizer)
-        view.addSubview(imgView)
-        player.listenerPos = imgView.center
-
+        //        let imgView = UIImageView(image: UIImage(named: "Headphone.png"))
+        //        imgView.frame = CGRect(x: UIScreen.mainScreen().bounds.size.width/2, y: UIScreen.mainScreen().bounds.size.height/2, width: 50, height: 50)
+        //        imgView.backgroundColor = UIColor.redColor()
+        //        imgView.userInteractionEnabled = true
+        //        imgView.addGestureRecognizer(panner)
+        //        imgView.addGestureRecognizer(tapGestureRecognizer)
+        //        view.addSubview(imgView)
+        //        player.listenerPos = imgView.center
     }
     
     // Load View
